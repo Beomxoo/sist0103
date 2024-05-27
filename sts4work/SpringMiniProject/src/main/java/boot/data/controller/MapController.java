514f50f045baf67324b5a6b5dfbc1b5de2aedbdb
Son <ssoo9271@naver.com>
2024-05-27 16:55:58 +0900

    0527

---
+++ sts4work/SpringMiniProject/src/main/java/boot/data/controller/MapController.java
@@ -0,0 +1,16 @@
+package boot.data.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MapController {
+	
+	@GetMapping("/load/map")
+	public String lamp()
+	{
+		return "/load/loadmap";
+	}
+	
+}
